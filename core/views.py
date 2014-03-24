6b5923a600a052eb58d2655da1029d6dacefc13c
Jack Montgomery <jackm321@gmail.com>
2014-03-24 07:33:07 -0400

    basic sending works

--- core/views.py
+++ core/views.py
@@ -1,4 +1,3 @@
-# Create your views here.
 from django.http import HttpResponse
 from core import models
 from django.shortcuts import get_object_or_404, render_to_response, redirect
@@ -6,16 +5,7 @@ from django.template import RequestContext
 from django.views.decorators.http import require_http_methods
 
 AUTH_TOKEN = "key-3ax6xnjp29jd6fds4gc373sgvjxteol0"
-FROM = "publichealth@pitt.edu"
-
-#################
-##### VIEWS #####
-#################
-
-# def index(request):
-#     data = {'projectName': 'mailer',
-#             'successMessage': "Great success!"}
-#     return render_to_response("index.html", data, context_instance=RequestContext(request))
+FROM = "Pitt Human Genetics <publichealth@pitt.edu>"
 
 @require_POST()
 def sendMessage(request):
@@ -23,19 +13,17 @@ def sendMessage(request):
   address = request.POST.address
   subject = request.POST.subject
   content = request.POST.content # stuff to fill render template with
+  status = doSend().status_code
+  return HttpResponse(status=status)
 
-###################
-##### HELPERS #####
-###################
 
-def send_complex_message():
+def doSend(to, subject, templateName, content):
+    template = get_template(templateName)
+    message = template.render(content)
     return requests.post(
-        "https://api.mailgun.net/v2/samples.mailgun.org/messages",
+        "https://api.mailgun.net/v2/messages",
         auth=("api", AUTH_TOKEN),
-        data={"from": "Excited User <me@samples.mailgun.org>",
-              "to": "foo@example.com",
-              "cc": "baz@example.com",
-              "bcc": "bar@example.com",
-              "subject": "Hello",
-              "text": "Testing some Mailgun awesomness!",
-              "html": "<html>HTML version of the body</html>"})
+        data={"from": FROM,
+              "to": to,
+              "subject": subject,
+              "html": message})
